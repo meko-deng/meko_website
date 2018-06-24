46e7a8780ded6c8d888daacd820669a75fe63bc9
Meko Deng <mekodeng94@gmail.com>
2018-06-24 09:44:28 -0400

    updating dependencies

---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'Config_management',
+    //   component: Config_management,
+    // },
+    // {
+    //   path: '/Default_config_table',
+    //   name: 'Default_config_table',
+    //   component: Default_config_table,
+    // }   
+  ]
+})
