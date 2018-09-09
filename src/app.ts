6a9761114f32aef3ac63baab47be2c0bc6303189
Meko Deng <mekodeng94@gmail.com>
2018-09-08 23:27:40 -0400

    adding google analytics vue-analytics plugin

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import App from './App.vue'
 import router from './router'
 import store from './store/simple_store'
@@ -6,6 +7,11 @@ import '../assets/app.css'
 import './assets/main_logo.css'
 import './assets/pages/page_header_animation.css'
 
+Vue.use(VueAnalytics, {
+  id: 'UA-125473579-1',
+  router,
+  checkDuplicatedScript:true
+})
 new Vue({
   el: '#app',
   router,
